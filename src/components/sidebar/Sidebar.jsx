a9d7b7f0769155beee0c2bb88ae208ce0126f51e
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-02-03 08:04:23 +0530

    8:02

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./sidebar.css";
 import SidebarButton from "./SidebarButton";
 import { MdFavorite } from "react-icons/md";
@@ -6,48 +6,47 @@ import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { AiFillHome } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
-import {addProfile } from "../../axios/user.axios";
-import { useSelector,useDispatch  } from 'react-redux';
+import { addProfile } from "../../axios/user.axios";
+import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../../axios/auth.axios";
 import { useNavigate } from "react-router-dom";
-
-
-
 
 // import apiClient from "../../spotify";
 
 export default function Sidebar() {
   const { user } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   const [image, setImage] = useState(
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
   );
   const Logout = (e) => {
     e.preventDefault();
-        dispatch({
-          type: "LOGOUT_USER",
-        });
-        navigate("/login");
+    dispatch({
+      type: "LOGOUT_USER",
+    });
+    navigate("/login");
   };
   const handleSubmit = (e) => {
-		// e.preventDefault();
+    // e.preventDefault();
 
-		const formData = new FormData();
-		formData.append('image', e);
-		formData.append('userId', user.user.user_id);
+    const formData = new FormData();
+    formData.append("userId", user.user.user_id);
+    formData.append("image", e);
 
-    
-		addProfile(formData, user && user.token).then((res) => {
+    addProfile(formData, user && user.token).then((res) => {
       console.log(res.data);
-		});
-
-	};
+    });
+  };
   return (
     <div className="sidebar-container">
       <label>
-        <input type="file" onChange={(e) => handleSubmit(e.target.files[0])} hidden />
+        <input
+          type="file"
+          onChange={(e) => handleSubmit(e.target.files[0])}
+          hidden
+        />
         <img src={image} className="profile-img" alt="profile" />
       </label>
 
