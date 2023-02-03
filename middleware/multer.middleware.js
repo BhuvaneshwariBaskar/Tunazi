c849a705469fa85daf1aef9a94c286ed0784397f
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-02-03 08:02:34 +0530

    8:02

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,16 +1,18 @@
-const multer = require('multer')
-const fs = require('fs')
+const multer = require("multer");
+const fs = require("fs");
 
 exports.imageUpload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            console.log(req.body);
-            path = `profile/${req.body.userId}`
-            fs.mkdirSync(path, { recursive: true })
-            cb(null, path)
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname)
-        },
-    }), limits: { fieldSize: 2 * 1024 * 1024 }
-})
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      console.log(file);
+      path = `public/users/${req.body.userId}`;
+      req.body.path = path;
+      fs.mkdirSync(path, { recursive: true });
+      cb(null, path);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+  limits: { fieldSize: 2 * 1024 * 1024 },
+});
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,29 @@
-
+const cloudinary = require("cloudinary");
 const express = require('express');
 const {profilePicPost} = require('../controllers/user.controller');
 const {validateUser}=require('../middleware/middleware')
 const {imageUpload}=require('../middleware/multer.middleware')
-
 const router = express.Router();
+const db = require("../database");
 
-// router.route('/profile/:id_user/:filename').get(profilePic)
 router.route('/addProfile').post(validateUser, imageUpload.single('image'), profilePicPost)
 
+
+cloudinary.config({
+  cloud_name: "deak6nhde",
+  api_key: "119724621868458",
+  api_secret: "skaJ6vB_l-p4L7YK0TYwzPm7dt8",
+});
+
+router.route('/musicdata').get(async(req,res)=>{
+    console.log("ok");
+    await db.query("SELECT * FROM happyhits", (err, response) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).json(response);
+        }
+      });
+})
 
 module.exports = router;
