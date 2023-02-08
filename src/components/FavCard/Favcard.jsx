01bbebed3adfb35001a6ec4d4b53dd5fc46af835
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-02-08 19:55:51 +0530

    Changes

--- src/components/FavCard/Favcard.jsx
+++ src/components/FavCard/Favcard.jsx
@@ -5,13 +5,19 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import { updateFavPost } from "../../axios/user.axios";
 import { useDispatch } from "react-redux";
 import "./Favcard.css";
+import { json } from "react-router-dom";
 
 const Favcard = ({ music, user }) => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const addToFav = async (music_id) => {
-    const favorites = [music_id, ...JSON.parse(user.favorites)];
-    console.log("🚀 ~ file: Smallcard.jsx:15 ~ addToFav ~ favorites", favorites)
+    const favorites = JSON.parse(user.favorites)
+      ? [music_id, ...JSON.parse(user.favorites)]
+      : [music_id];
+    console.log(
+      "🚀 ~ file: Smallcard.jsx:15 ~ addToFav ~ favorites",
+      favorites
+    );
     await updateFavPost(user.user_id, favorites, user.token).then((res) => {
       dispatch({
         type: "CREATE_USER",
@@ -20,9 +26,9 @@ const dispatch = useDispatch();
     });
   };
   const removeFav = async (music_id) => {
-    const favorites = JSON.parse(user.favorites).filter(
-      (fav) => fav !== music_id
-    );
+    const favorites = JSON.parse(user.favorites)
+      ? JSON.parse(user.favorites).filter((fav) => fav !== music_id)
+      : [];
     await updateFavPost(user.user_id, favorites, user.token).then((res) => {
       dispatch({
         type: "CREATE_USER",
@@ -33,27 +39,26 @@ const dispatch = useDispatch();
   return (
     <section className="fav-card">
       <div className="fav-cardsec">
-        <img
-          src={music.thumbnail}
-          alt=""
-        />
+        <img src={music.thumbnail} alt="" />
       </div>
       <div className="fav-content">
         <div className="fav-innercontent">
           <div className="favtext">
-            <p className="fav-title">
-              
-              {music.title}
-            </p>
+            <p className="fav-title">{music.title}</p>
             <p className="fav-artist">
-             
               {music.artist}-{music.year}
             </p>
           </div>
           {JSON.parse(user.favorites).some((id) => id === music.music_id) ? (
-            <IoIosHeart className="fav-Likebutton" onClick={() => removeFav(music.music_id)}/>
+            <IoIosHeart
+              className="fav-Likebutton"
+              onClick={() => removeFav(music.music_id)}
+            />
           ) : (
-            <IoMdHeartEmpty className="fav-Likebutton" onClick={() => addToFav(music.music_id)} />
+            <IoMdHeartEmpty
+              className="fav-Likebutton"
+              onClick={() => addToFav(music.music_id)}
+            />
           )}
 
           <AiFillPlayCircle className="fav-playbutton" />
