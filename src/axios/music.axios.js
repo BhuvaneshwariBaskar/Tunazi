a514e500169f224fff426664b28fbf17bbd874f8
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-02-09 08:56:05 +0530

    Changes

--- src/axios/music.axios.js
+++ src/axios/music.axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import {base_url} from "../utils/constants.util"
+
 
 export const fetchMusic = async (token) =>
-  await axios.get(`http://localhost:8000/api/music`, {
+  await axios.get(`${base_url}/music`, {
     headers: { Authorization: `Bearer ${token}` },
   });
