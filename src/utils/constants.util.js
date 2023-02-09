5cf7f6637c3a42f1524833a83ddb18fef7e77752
lohithsrk <srklohith05@gmail.com>
2023-02-09 22:14:51 +0530

    chanegs

--- src/utils/constants.util.js
+++ src/utils/constants.util.js
@@ -1,2 +1,3 @@
-export const base_url = "http://194.163.43.5/api";
+// export const base_url = "http://194.163.43.5/api";
+export const base_url = "https://tunazi-server.onrender.com/api";
 // export  const base_url='http://localhost:8000/api'
