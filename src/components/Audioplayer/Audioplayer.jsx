f56c6d5ea9f17037df8aab12a5fa4e6703589b6a
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-02-10 06:28:51 +0530

    Changes

--- src/components/Audioplayer/Audioplayer.jsx
+++ src/components/Audioplayer/Audioplayer.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "./audioplayer.css";
-import { updateHistory } from "../../axios/user.axios";
+import { updateHistory ,updateFavPost} from "../../axios/user.axios";
 import { useDispatch } from "react-redux";
+
+import { IoIosHeart } from 'react-icons/io';
+import { IoMdHeartEmpty } from 'react-icons/io';
 
 const Audioplayer = ({ cursong, user }) => {
   const dispatch = useDispatch();
-
+  // const addToFav = async (cursong.music_id) => {
+	// 	const favorites = JSON.parse(user.favorites)
+	// 		? [cursong.music_id, ...JSON.parse(user.favorites)]
+	// 		: [cursong.music_id];
+	// 	await updateFavPost(user.user_id, favorites, user.token).then((res) => {
+	// 		dispatch({
+	// 			type: 'CREATE_USER',
+	// 			payload: { ...user, favorites: JSON.stringify(favorites) }
+	// 		});
+	// 	});
+	// };
+	// const removeFav = async (cursong.music_id) => {
+	// 	const favorites = JSON.parse(user.favorites)
+	// 		? JSON.parse(user.favorites).filter((fav) => fav !== cursong.music_id)
+	// 		: [];
+	// 	await updateFavPost(user.user_id, favorites, user.token).then((res) => {
+	// 		dispatch({
+	// 			type: 'CREATE_USER',
+	// 			payload: { ...user, favorites: JSON.stringify(favorites) }
+	// 		});
+	// 	});
+	// };
   const handlePlay = async () => {
     const history = JSON.parse(user.history) ? JSON.parse(user.history) : [];
     history.unshift(cursong.music_id);
@@ -18,12 +42,25 @@ const Audioplayer = ({ cursong, user }) => {
     });
   };
   return (
-    <>
+    <section className="audiocon">
       <div className="imgname">
-        <div className="imgcon">
           <img src={cursong.thumnail} alt="" className="cursongimage" />
+        <div className="Namecon">
+          <h1 className="cursongname">{cursong.title}</h1>
+          <h1 className="cursongartist">{cursong.artist}</h1>
         </div>
-        div.
+        {/* {JSON.parse(user.favorites) &&
+					JSON.parse(user.favorites).some((id) => id === music.music_id) ? (
+						<IoIosHeart
+							className='fav-Likebutton'
+							onClick={() => removeFav(music.music_id)}
+						/>
+					) : (
+						<IoMdHeartEmpty
+							className='fav-Likebutton'
+							onClick={() => addToFav(music.music_id)}
+						/>
+					)} */}
       </div>
       <AudioPlayer
         src={cursong && cursong.link}
@@ -31,7 +68,7 @@ const Audioplayer = ({ cursong, user }) => {
         autoPlay={false}
         // other props here
       />
-    </>
+    </section>
   );
 };
 
--- src/components/FavCard/Favcard.jsx
+++ src/components/FavCard/Favcard.jsx
@@ -1,66 +1,66 @@
-import React, { useState } from 'react';
-import { AiFillPlayCircle } from 'react-icons/ai';
-import { IoIosHeart } from 'react-icons/io';
-import { IoMdHeartEmpty } from 'react-icons/io';
-import { updateFavPost } from '../../axios/user.axios';
-import { useDispatch } from 'react-redux';
-import './Favcard.css';
+import React, { useState } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { IoIosHeart } from "react-icons/io";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { updateFavPost } from "../../axios/user.axios";
+import { useDispatch } from "react-redux";
+import "./Favcard.css";
 
 const Favcard = ({ music, user }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const addToFav = async (music_id) => {
-		const favorites = JSON.parse(user.favorites)
-			? [music_id, ...JSON.parse(user.favorites)]
-			: [music_id];
-		await updateFavPost(user.user_id, favorites, user.token).then((res) => {
-			dispatch({
-				type: 'CREATE_USER',
-				payload: { ...user, favorites: JSON.stringify(favorites) }
-			});
-		});
-	};
-	const removeFav = async (music_id) => {
-		const favorites = JSON.parse(user.favorites)
-			? JSON.parse(user.favorites).filter((fav) => fav !== music_id)
-			: [];
-		await updateFavPost(user.user_id, favorites, user.token).then((res) => {
-			dispatch({
-				type: 'CREATE_USER',
-				payload: { ...user, favorites: JSON.stringify(favorites) }
-			});
-		});
-	};
-	return (
-		<section className='fav-card'>
-			<div className='fav-cardsec'>
-				<img src={music.thumbnail} alt='' />
-			</div>
-			<div className='fav-content'>
-				<div className='fav-innercontent'>
-					<div className='favtext'>
-						<p className='fav-title'>{music.title}</p>
-						<p className='fav-artist'>
-							{music.artist}-{music.year}
-						</p>
-					</div>
-					{JSON.parse(user.favorites) &&
-					JSON.parse(user.favorites).some((id) => id === music.music_id) ? (
-						<IoIosHeart
-							className='fav-Likebutton'
-							onClick={() => removeFav(music.music_id)}
-						/>
-					) : (
-						<IoMdHeartEmpty
-							className='fav-Likebutton'
-							onClick={() => addToFav(music.music_id)}
-						/>
-					)}
+  const addToFav = async (music_id) => {
+    const favorites = JSON.parse(user.favorites)
+      ? [music_id, ...JSON.parse(user.favorites)]
+      : [music_id];
+    await updateFavPost(user.user_id, favorites, user.token).then((res) => {
+      dispatch({
+        type: "CREATE_USER",
+        payload: { ...user, favorites: JSON.stringify(favorites) },
+      });
+    });
+  };
+  const removeFav = async (music_id) => {
+    const favorites = JSON.parse(user.favorites)
+      ? JSON.parse(user.favorites).filter((fav) => fav !== music_id)
+      : [];
+    await updateFavPost(user.user_id, favorites, user.token).then((res) => {
+      dispatch({
+        type: "CREATE_USER",
+        payload: { ...user, favorites: JSON.stringify(favorites) },
+      });
+    });
+  };
+  return (
+    <section className="fav-card">
+      <div className="fav-cardsec">
+        <img src={music.thumbnail} alt="" />
+      </div>
+      <div className="fav-content">
+        <div className="fav-innercontent">
+          <div className="favtext">
+            <p className="fav-title">{music.title}</p>
+            <p className="fav-artist">
+              {music.artist}-{music.year}
+            </p>
+          </div>
+          {JSON.parse(user.favorites) &&
+          JSON.parse(user.favorites).some((id) => id === music.music_id) ? (
+            <IoIosHeart
+              className="fav-Likebutton"
+              onClick={() => removeFav(music.music_id)}
+            />
+          ) : (
+            <IoMdHeartEmpty
+              className="fav-Likebutton"
+              onClick={() => addToFav(music.music_id)}
+            />
+          )}
 
-					<AiFillPlayCircle className='fav-playbutton' />
-				</div>
-			</div>
-		</section>
-	);
+          <AiFillPlayCircle className="fav-playbutton" />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Favcard;
