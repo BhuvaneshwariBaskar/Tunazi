2f35103b7c1e9e533804141d38c8d61d75f63c0b
lohithsrk <srklohith05@gmail.com>
2023-03-08 13:55:08 +0530

    changes

--- src/components/Audioplayer/Audioplayer.jsx
+++ src/components/Audioplayer/Audioplayer.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import AudioPlayer from "react-h5-audio-player";
-import "./audioplayer.css";
-import { updateHistory } from "../../axios/user.axios";
-import { useDispatch } from "react-redux";
+import React from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import './audioplayer.css';
+import { updateHistory } from '../../axios/user.axios';
+import { useDispatch } from 'react-redux';
 
 // import { IoIosHeart } from 'react-icons/io';
 // import { IoMdHeartEmpty } from 'react-icons/io';
 
-const Audioplayer = ({ cursong, user }) => {
-  const dispatch = useDispatch();
+const Audioplayer = ({ cursong, user, isNavOpened }) => {
+	const dispatch = useDispatch();
 
-  const handlePlay = async () => {
-    const history = JSON.parse(user.history) ? JSON.parse(user.history) : [];
-    history.unshift(cursong.music_id);
-    await updateHistory(history, user.user_id, user.token).then((res) => {
-      dispatch({
-        type: "CREATE_USER",
-        payload: { ...user, history: JSON.stringify(history) },
-      });
-    });
-  };
-  return (
-    <>
-      {/* <div className="imgname">
+	const handlePlay = async () => {
+		const history = JSON.parse(user.history) ? JSON.parse(user.history) : [];
+		history.unshift(cursong.music_id);
+		await updateHistory(history, user.user_id, user.token).then((res) => {
+			dispatch({
+				type: 'CREATE_USER',
+				payload: { ...user, history: JSON.stringify(history) }
+			});
+		});
+	};
+	return (
+		<>
+			{/* <div className="imgname">
         <div className="imgcon">
           <img
             src={cursong && cursong.thumnail}
@@ -32,15 +32,19 @@ const Audioplayer = ({ cursong, user }) => {
         </div>
         
       </div> */}
-      <AudioPlayer
-        src={cursong && cursong.link}
-        onPlay={handlePlay}
-        autoPlay={false}
-
-        // other props here
-      />
-    </>
-  );
+			<AudioPlayer
+				src={cursong && cursong.link}
+				onPlay={handlePlay}
+				autoPlay={false}
+				style={{
+					paddingLeft:
+						window.innerWidth <= 640 ? (isNavOpened ? '100px' : '0') : '100px'
+					// window.innerWidth <= 640 ? '100%' : !isNavOpened ? '93%' : '100%'
+				}}
+				// other props here
+			/>
+		</>
+	);
 };
 
 export default Audioplayer;
