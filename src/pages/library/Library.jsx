480221d997824ae9432c0bcc50f1dd5105cfc412
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-03-09 11:26:12 +0530

    Changes

--- src/pages/library/Library.jsx
+++ src/pages/library/Library.jsx
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from "react";
-import Lib from "../../components/lib/Lib"
-import {Music} from "../../db/music"
+import React from "react";
+import Lib from "../../components/lib/Lib";
+
+// import { Music } from "../../db/music";
 
 // import { getSadSong, getTrendingSong } from "../../utils/data";
 import "./library.css";
 
-const Library = ( {Music} ) => {
+const Library = ({ music,user}) => {
   return (
     <section className="lib">
-      
-      <div>
-        <Lib music={Music}/>
-        
+       {/* <h1 className="Recently-title">Your playlist</h1>
+          <div class="grid-item">
+            {music.map((mus, index) => {
+          
+              return <Lib music={mus} key={index} user={user} />;
+            })}
+          </div> */}
+      <div className="Add">
+        <img src="https://img.icons8.com/carbon-copy/100/#fff/plus-2-math.png" alt=""/>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Library
+export default Library;
--- src/utils/constants.util.js
+++ src/utils/constants.util.js
@@ -1,3 +1,3 @@
-export const base_url = "http://13.232.73.158/api";
-// export const base_url = "https://tunazi-server.onrender.com/api";
+// export const base_url = "http://13.232.73.158/api";
+export const base_url = "https://tunazi-server.onrender.com/api";
 // export const base_url = "http://localhost:8080/api";
